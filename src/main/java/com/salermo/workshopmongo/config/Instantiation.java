2aa507773436d5d9cd3a07129228391f91dbf945
gbsalermo <enzogabrielskull@gmail.com>
2026-02-04 22:38:33 -0300

    Criando entity Post e aninhando com User

--- src/main/java/com/salermo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/salermo/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.salermo.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.salermo.workshopmongo.domain.Post;
 import com.salermo.workshopmongo.domain.User;
+import com.salermo.workshopmongo.repository.PostRepository;
 import com.salermo.workshopmongo.repository.UserRepository;
 
 //Classe de operação para instanciar a base de dados
@@ -15,19 +19,34 @@ import com.salermo.workshopmongo.repository.UserRepository;
 public class Instantiation implements CommandLineRunner {
 
 	@Autowired//Injeção de dependencia
+	private PostRepository postRepository;
+	
+	@Autowired//Injeção de dependencia
 	private UserRepository userRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		//Primeiro limpo o repositorio
 		userRepository.deleteAll();
+		postRepository.deleteAll();
+		
 		//Instancio os usuarios
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, Abraços!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		
 		//Salvo eles
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
+		
 		
 		
 	}
---
+++ src/main/java/com/salermo/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.salermo.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.salermo.workshopmongo.domain.Post;
+
+//Repositorio da classe para o banco de dados
+@Repository	
+public interface PostRepository extends MongoRepository<Post, String>{ //Tenho que por qual a clase e o tipo do id
+
+}
